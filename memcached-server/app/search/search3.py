9d519abd93fe7e8efd7bd2a8554d98c7b26aa33d
Angeles Navarrete <106721742+AngelesNav@users.noreply.github.com>
2023-09-26 23:34:58 -0300

    Update search3.py

--- memcached-server/app/search/search3.py
+++ memcached-server/app/search/search3.py
@@ -3,14 +3,12 @@ import numpy as np
 from find_car_by_id import find_car_by_id
 import memcache
 
-
-
 class CacheClient:
     def __init__(self, host="memcached", port=11211):
         self.memcached_client = memcache.Client([f"{host}:{port}"])
-        self.cache_hits = 0
-        self.total_searches = 0
-        
+        self.cache_hits = 0  # Contador de búsquedas resueltas en caché
+        self.cache_misses = 0  # Contador de búsquedas en caché que resultan en fallos
+        self.total_searches = 0  # Contador del total de búsquedas
 
     def get(self, key, simulated=False):
         start_time = time.time()  # Inicio del temporizador
@@ -19,7 +17,8 @@ class CacheClient:
         if value is not None:  # Comprobar si hay un valor, en lugar de 'exists'
             elapsed_time = time.time() - start_time  # Calcula el tiempo transcurrido
             print(f"Time taken (cache): {elapsed_time:.5f} seconds")
-            self.cache_hits += 1 
+            self.cache_hits += 1
+            self.total_searches += 1
             return value
 
         else:
@@ -41,18 +40,17 @@ class CacheClient:
                     # add delay to time just sum
                     elapsed_time += delay
                 print(f"Time taken (JSON + delay): {elapsed_time:.5f} seconds")
-                
+                self.cache_misses += 1
+                self.total_searches += 1
                 return value
             else:
                 elapsed_time = time.time() - start_time  # Calcula el tiempo transcurrido
                 print(f"Time taken: {elapsed_time:.5f} seconds")
                 print("Key not found.")
+                self.total_searches += 1
                 return None 
-           
-        
 
-            
-    def simulate_scenarios(self, n_searches=100, constant_frequency = False):
+    def simulate_scenarios(self, n_searches=100, constant_frequency=False):
         keys_to_search = [f"{i}" for i in np.random.randint(1, 101, n_searches)]
 
         # Métricas
@@ -68,27 +66,29 @@ class CacheClient:
             print(f"Searching : {count}/{n_searches}")
             start_time = time.time()
             time_without_cache += 3 + 0.001  # Estimado de tiempo de búsqueda en JSON
-            self.get(key, simulated = constant_frequency)
+            self.get(key, simulated=constant_frequency)
             elapsed_time = time.time() - start_time
             time_with_cache += elapsed_time
 
             if elapsed_time < 1:
                 avoided_json_lookups += 1
 
-            self.total_searches += 1    
-
         time_saved = time_without_cache - time_with_cache
         print(f"\nTime saved thanks to cache: {time_saved:.2f} seconds")
         print(f"Number of times JSON lookup was avoided: {avoided_json_lookups}")
-        
+
+        # Calcular la tasa de fallos (miss rate) y el hit rate del caché
+        miss_rate = self.cache_misses / self.total_searches
         hit_rate = self.cache_hits / self.total_searches
-        print(f"Hit Rate del cache: {hit_rate:.2%}")
+
+        print(f"Miss Rate (Cache): {miss_rate:.2%}")
+        print(f"Hit Rate (Cache): {hit_rate:.2%}")
 
 if __name__ == '__main__':
     client = CacheClient()
 
     while True:
-        print("\nMenu CON cache:")
+        print("\nMenu CON Memcache:")
         print("1. Get")
         print("2. Simulate Searches (Normal Distribution)")
         print("3. Simulate Searches (Constant Frequency)")
